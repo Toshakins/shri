8aff7f969d564ad3550aeeab064186f1a00769e6
Toshakins <toshakins@gmail.com>
2013-03-02 03:16:01 +0200

    Basically bookmarkletted. Loader + div

--- 02-js/bookmarklet.js
+++ 02-js/bookmarklet.js
@@ -1,14 +1,44 @@
-$('body > *').css({'display': 'none'});
-$photos = $(".photo > a");
-var thumbnails = new Array();
-var large = new Array();
-$photos.find("img").each(function() {
-	thumbnails.push($(this).attr("src"));
-});
-/* Here you can only select 5.jpg - so no need in $.get() */
-$photos.each(function() {
-	$.get($(this).attr("href"))
-	.then(function (data) {
-		large.push($(data).find("#mainphoto").attr("src"));
-	});
-});
+javascript:
+(function () {
+	var v = "1.8.0";
+	if (window.jQuery === undefined/* || window.jQuery.fn.jquery < v*/) {
+
+		var done = false;
+		script = document.createElement("script");
+		script.src="http://code.jquery.com/jquery-1.9.1.min.js";
+		script.onload = script.onreadystatechange = function(){
+			if (!done && (!this.readyState || this.readyState == "loaded"
+				|| this.readyState == "complete")) {
+				done = true;
+				init();
+			}
+		};
+		document.getElementsByTagName("head")[0].appendChild(script);
+	}
+	init();
+
+	function init() {
+		
+		nextPage = (function () {
+			var page = 1;
+			return (function() {return page++;});
+		})();
+
+		base = "http://500px.com/photos?page=";
+		var photos = new Array();
+		$.get(base + nextPage())
+		.then(function (data) {
+		$(data).find(".photo img").each(function() {
+			/* todo: replace w/ regexp */
+			photos.push($(this).attr("src").replace("3.jpg", ""));
+		});
+		$("body").append("<div id='desk'></div>");
+		$desk = $("#desk");
+		$desk.css({
+			'overflow'  : 'hidden',
+			'background': 'gray'
+		});
+		console.log($desk);
+		});
+	}
+})();
